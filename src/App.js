6e742f7d5a09d4a25a6b93b64e06b307fb2adc0b
Salaudev <pysalau2909@gmail.com>
2022-02-23 23:38:34 +0500

    Vendor Cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
 import SalePage from "./pages/salepage";
+import AllVendors from "./pages/Vendors/AllVendors";
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
@@ -9,7 +10,9 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      {/* <Home /> */}
+
+      <AllVendors />
     </>
   );
 }
