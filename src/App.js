e9b31feaab712733cd48ccf18fb4a3f3b1c47a6f
Abdurakhimbek <nurmatov.abdurahim98@mail.ru>
2022-02-23 23:37:51 +0500

    theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { createContext } from "react";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
 import SalePage from "./pages/salepage";
+import { createContext, useContext, useMemo, useState } from "react";
+import { ThemeProvider, useTheme } from "@mui/material/styles";
+import customTheme from "./Theme";
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
-function App() {
+export default function App() {
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => customTheme(mode), [mode]);
+  console.log(theme);
+
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <section style={{ background: theme.palette.background.default }}>
+          <Header />
+          <Home />
+        </section>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
-export default App;
+export { ColorModeContext };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import MainApp from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
