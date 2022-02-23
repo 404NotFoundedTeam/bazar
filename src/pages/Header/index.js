49415aa2c2dbce15dcf9b4bfc7ebd38493b4d973
AliwerDev <alwerdev@gmail.com>
2022-02-23 17:32:25 +0500

    heloo

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,8 +1,11 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Box component={"header"}>
+        
+    </Box>
   )
 }
 
