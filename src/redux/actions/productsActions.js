d98ac1a33746d4626316d0af943f7680eae7d0f7
AliwerDev <alwerdev@gmail.com>
2022-03-07 16:55:12 +0500

    sadfsfdafsf

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -1,5 +1,6 @@
 import { dispatch } from "../store";
 import { FILTER_PRODUCT } from "../types";
+import { addProductToCategory } from "./categoryAction";
 
 export const changeOpenCart = (action) => {
   dispatch({ type: FILTER_PRODUCT, payload: action });
