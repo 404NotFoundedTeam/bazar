565627d12a36007929878d001bbeccdc98e63d11
AliwerDev <alwerdev@gmail.com>
2022-03-09 10:30:33 +0500

    fdasfasdf

---
+++ src/components/card/InfoCard.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+import React from "react";
+import ShadowBox from "../ShadowBox";
+
+const InfoCard = ({ icon, title, desc }) => {
+  return (
+    <ShadowBox>
+      <Box></Box>
+    </ShadowBox>
+  );
+};
+
+export default InfoCard;
